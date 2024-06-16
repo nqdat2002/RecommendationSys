cf80fa2a4435ce1e56889d89bb1b474cda1b73b4
nqdat2002 <datfeed.2706@gmail.com>
2024-06-16 22:15:02 +0700

    root folder update

--- server/app.py
+++ server/app.py
@@ -10,18 +10,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-cred = credentials.Certificate({
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
-})
+cred = credentials.Certificate(os.path.join(os.getcwd(), 'cred', 'credentials.json'))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
